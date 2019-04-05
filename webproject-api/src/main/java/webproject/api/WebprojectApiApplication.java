d1aa898a421cf77b60d657d3fa6d81eb56742df4
Bogdan <b_mamay3@ukr.net>
2019-04-05 11:11:36 +0300

    Added api an ui

---
+++ webproject-api/src/main/java/webproject/api/WebprojectApiApplication.java
@@ -0,0 +1,13 @@
+package webproject.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojectApiApplication.class, args);
+	}
+
+}
