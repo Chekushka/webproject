6c4cfbdec39aa3313ebc32d1e83b791146abc4be
Serhii <chekun.sergiy@gmail.com>
2019-05-09 23:09:47 +0300

    Added registration

--- webproject-ui/src/App.js
+++ webproject-ui/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import ExpenceList from './pages/expences/ExpenceList';
+import RegistrationForm from './pages/account/RegistrationForm';
+import Notfound from './pages/common/NotFound';
 
 class App extends Component {
+
+  constructor() {
+      super();
+      console.log("[App] constructor");
+  }
+
+  componentDidMount() {
+    console.log("[App] componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("[App] componentWillUnmount");
+  }
+
   render() {
+    console.log("[App] render");
+    
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Book Exchanger</h1>  
+        <div>
+          <Link to="/register">Register</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={ExpenceList} />
+          <Route path="/register" component={RegistrationForm} />
+          <Route component={Notfound} />          
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
